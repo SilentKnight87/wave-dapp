75a76b2ad25b436996c11991dac8121720e1d6fb
Peter B <petrojoash@yahoo.com>
2022-02-10 23:06:25 -0500

    scripts to run contracts, added func for contracts

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,36 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    address owner;
+    address [] public wavedAtMe;
+
+
+
     constructor() {
-        console.log("Yo yo, I am a smaart contract and I am really smart");
+        owner = msg.sender;
+        console.log("I am a smart contract, and yes I am super smart!");
     }
+
+    function wave() public {
+        if (owner == msg.sender){
+            console.log("you waved at yourself");
+            totalWaves += 1;
+        }else{
+            totalWaves += 1;
+            wavedAtMe.push(payable(msg.sender));
+            console.log("%s has waved!", msg.sender);
+            }
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
+    function allWhoWaved() public view returns (address [] memory) {
+        return wavedAtMe;
+    }
+
+
 }
